4844bcca8481a8d793966a07368186f033ffe163
erashishmodi <i_m_ashish@yahoo.com>
2014-07-13 23:53:10 +0530

    Create TutorialApplication1.cs

---
+++ TutorialApplication1.cs
@@ -0,0 +1,16 @@
+/*In this we will learn about to writing program in c# and compile it using Press F5(Debug mode) and also check With CTR+F5 keay(Without debug mode)*/
+
+
+
+
+
+namespace TutorialApplication1
+{
+    public class HelloWorld
+    {
+         static void  Main(string[] args)
+        {
+            System.Console.WriteLine("HELLO ,c#!");
+        }
+    }
+}
