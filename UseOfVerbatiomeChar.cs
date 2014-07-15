ee2c98474221177933d0b1afd385d3ea167ae403
erashishmodi <i_m_ashish@yahoo.com>
2014-07-15 23:08:49 +0530

    Update UseOfVerbatiomeChar.cs

--- UseOfVerbatiomeChar.cs
+++ UseOfVerbatiomeChar.cs
@@ -5,14 +5,18 @@ namespace UseOfVerbatime
 {
     class UseOfVerbatime
     {
-        static void Main()
+         static void Main()
         {
             System.String path1="c:\turboc\newfolder";
             System.String path2="c:\\turboc\\newfolder";//<------Solution Of Above Problem
             System.String path3 = @"c:\turboc\newfolder";//<------Solution Of Above Problem
+            string address = @"abc nagar;
+near def gate;
+opp.xyz location";
             System.Console.WriteLine("Path1 : "+path1);
             System.Console.WriteLine("Path2 : "+path2);
             System.Console.WriteLine("Path3 : "+path3);
+            System.Console.WriteLine("Address :" + address);
         }
  
     }
