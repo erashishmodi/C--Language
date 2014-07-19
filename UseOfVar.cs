4eb8f499e2aa83f7253c754b56b241598b04aa44
erashishmodi <i_m_ashish@yahoo.com>
2014-07-19 17:28:07 +0530

    Create UseOfVar.cs

---
+++ UseOfVar.cs
@@ -0,0 +1,15 @@
+//In this program we will see the use of "var" as the local variable
+
+namespace UseOfVar
+{
+    class MainClass
+    {
+        static void Main()
+        {
+            var IntegerType = 10;
+            var StringType = "Hello";
+            System.Console.WriteLine("INTEGER USING VAR : " + IntegerType);
+            System.Console.WriteLine("STRING USING VAR : " + StringType);
+        }
+    }
+}
