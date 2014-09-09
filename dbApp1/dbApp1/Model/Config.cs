e8ac19594784f94094dc464679a1e7ac1b2188bf
Kanaiyalal Prajapati <adatapost@gmail.com>
2014-09-09 09:26:45 +0530

    added files

---
+++ dbApp1/dbApp1/Model/Config.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace dbApp1.Model
+{
+    public class Config
+    {
+        public static string CnStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\csnet\C--Language\dbApp1\dbApp1\SampleDb.mdf;Integrated Security=True";
+    }
+}
