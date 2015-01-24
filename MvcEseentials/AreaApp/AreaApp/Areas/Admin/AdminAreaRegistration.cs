cb405004e78595c00f5024e6cf35e883ec4c24ec
Ashish <i_m_ashish@yahoo.com>
2015-01-25 01:10:18 +0530

    Updated

---
+++ MvcEseentials/AreaApp/AreaApp/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AreaApp.Areas.Admin
+{
+    public class AdminAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Admin_default",
+                "Admin/{controller}/{action}/{id}",
+                new {Controller="User",action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MvcEseentials/UploadFile/UploadFile/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if(file!=null)
+            {
+                
+                var path = Server.MapPath("~/Files/"+file.FileName);
+                file.SaveAs(path);
+                ViewBag.Message = file.FileName + " of " + file.ContentType + " type and " + file.ContentLength + " of size is uploaded";                
+            }
+           
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Addcat
+
+    }
+}
