1df47480473862e73f8c158d45c29a3603409a23
Ashish <i_m_ashish@yahoo.com>
2015-06-17 14:54:11 +0530

    update

---
+++ MvcEseentials/CookieApp/CookieApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CookieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {return Content("Home Page");}
+        public void Create()
+        {CookieApp.Util.CookieWrapper.CreateCookie("UserName","Ashish Modi");}
+        public void Delete()
+        {CookieApp.Util.CookieWrapper.DeleteCookie("UserName");}
+        public ActionResult Read()
+        {return Content(CookieApp.Util.CookieWrapper.ReadCookie("UserName"));}
+    }
+}
---
+++ MvcEseentials/CookieApp/CookieApp/Util/CookieWrapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CookieApp.Util
+{
+    public class CookieWrapper
+    {
+        public static void CreateCookie(String key, String Value)
+        {
+            HttpCookie c = new HttpCookie(key,Value);
+            c.Path = "/";
+            c.Expires = DateTime.Now.AddDays(7);
+            HttpContext.Current.Response.Cookies.Add(c);
+        }
+
+        public static String ReadCookie(String Key)
+        {
+            String Value = "";
+            if (HttpContext.Current.Request.Cookies[Key] != null)
+                Value = HttpContext.Current.Request.Cookies[Key].Value;
+            return Value;
+        }
+
+        public static void DeleteCookie(String Key)
+        {
+            HttpCookie c= new HttpCookie(Key,"");
+            c.Path = "/";
+            c.Expires = DateTime.Now.AddSeconds(-1);
+            HttpContext.Current.Response.Cookies.Add(c);
+        }
+    }
+}
